1c48a6dd90f579be156a935998ec1134750d9f73
KohNotoya2 <93630361+KohNotoya2@users.noreply.github.com>
2023-01-13 18:22:56 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function moveToBottle() {
+    window.location="bottle.html"
+}
+
+function moveToHighway() {
+    window.location="highway.html"
+}
+
+function moveToMountFuji() {
+    window.location="mount_fuji.html"
+}
+
+function moveToTajMahal() {
+    window.location="taj_mahal.html"
+}
+
+function moveToTimesSquare() {
+    window.location="times_square.html"
+}
